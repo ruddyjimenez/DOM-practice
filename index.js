5d690cd8bb86574cb8a7c7c0b65a2663b41beb61
Ruddy <ruddy.jm@usa.com>
2021-04-21 04:18:38 -0500

    update

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-let intro = document.getElementById("intro")
+let intro = document.getElementById('intro')
 
+let list = document.createElement('li')
+list.textContent = 'added List'
+let newList = document.querySelector('#list')
+newList.append(list)
 
+let paragraph = document.createElement('p')
+document.body.append(paragraph)
+paragraph.textContent = 'This is a paragraph'
+paragraph.style.background = 'burlywood'
+paragraph.style.fontSize = '30px'
+paragraph.style.textAlign = 'center'
