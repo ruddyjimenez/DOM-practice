f1790c56df76994d0b4f2e307dac9145f9e11572
Ruddy <ruddy.jm@usa.com>
2021-04-23 04:02:27 -0500

    update

---
+++ example.js
@@ -0,0 +1,11 @@
+var powerRangers = [
+    "Jason Lee Scott", 
+    "Kimberly Hart", 
+    "Zack Taylor", 
+    "Trini Kwan", 
+    "Billy Cranston"
+]
+
+for (var i = 0; i < powerRangers.length; i++) {
+    powerRangers[i].
+}
