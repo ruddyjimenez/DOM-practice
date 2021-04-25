2d86204be192843ca48953b4d9c54d9c120dfe77
Ruddy <ruddy.jm@usa.com>
2021-04-24 19:33:01 -0500

    update example

--- example.js
+++ example.js
@@ -23,3 +23,21 @@ var rangersList = document.getElementById("rangers")
 
 for (var i = 0; i < powerRangers.length; i++) {
     rangersList.innerHTML += "<li>" + powerRangers[i] + "</li>"
+}
+
+//alternative grabbing multiple objects inside the array
+
+var powerRangers = [
+    {name: "Jason Lee Scott", color: "Red"}, 
+    {name: "Kimberly Hart", color: "Pink"}, 
+    {name: "Zack Taylor", color: "Black"}, 
+    {name: "Trini Kwan", color: "Yellow"}, 
+    {name: "Billy Cranston", color: "Blue"},
+    {name: "Tommy Oliver", color: "Green"}
+]
+
+var rangersList = document.getElementById("rangers")
+
+for (var i = 0; i < powerRangers.length; i++) {
+    rangersList.innerHTML += "<li>" + powerRangers[i].name + '- ' + powerRangers[i].color + "</li>"
+}
