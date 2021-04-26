efe8ad06ea02216a779298d2e583fe6d0e644c56
Ruddy <ruddy.jm@usa.com>
2021-04-26 04:17:34 -0500

    button

---
+++ btn.js
@@ -0,0 +1,6 @@
+var input = document.querySelector('#inp')
+
+var btn = document.querySelector('#btn').addEventListener('click', function () {
+    // document.body.style.background = 'blue'
+    input.innerHTML += '<p>' + document.querySelector('#inp') + '</p>'
+    })
